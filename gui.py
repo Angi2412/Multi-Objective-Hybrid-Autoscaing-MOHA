978904545c1d2389258d58be1e83ff65daffb0e8
Angelina Horn <horn.angelina96@gmail.com>
2020-12-18 09:46:19 +0100

    locust

--- gui.py
+++ gui.py
@@ -1,18 +1,27 @@
-# This will be the gui for the sandbox
+# Copyright (c) 2020 Angelina Horn
+import os
+
 import PySimpleGUI as sg
+
 import sandbox
 
 sg.theme('Reddit')
 
 layout = [
-    [sg.Text('Please enter your microservice information')],
+    [sg.Text('Microservice configuration')],
     [sg.Text('Name:', size=(15, 1)), sg.InputText()],
-    [sg.Text('Port:', size=(15, 1)), sg.InputText()],
+    [sg.Text('Port:', size=(15, 1)), sg.Spin([i for i in range(0000, 9999)], initial_value=0000, size=(4, 1))],
     [sg.Text('Dockerfile:', size=(15, 1)), sg.In(size=(35, 1)), sg.FileBrowse()],
+    [sg.Text('API information')],
+    [sg.Text('Route:', size=(15, 1)), sg.InputText()],
+    [sg.Text('Attribute:', size=(15, 1)), sg.InputText()],
+    [sg.Text('Testfile:', size=(15, 1)), sg.In(size=(35, 1)),
+     sg.FileBrowse(file_types=(("Text Files", "*.txt"),), initial_folder=os.getcwd())],
     [sg.Submit(), sg.Cancel()]
 ]
 
 window = sg.Window('PodAutoScaler', layout)
 event, values = window.read()
-sandbox.execute(name=values[0], port=values[1], docker_path=values[2])
+sandbox.execute(name=values[0], port=values[1], docker_path=values[2], route=values[3], input_type=values[4],
+                testfile_path=values[5])
 window.close()
--- sandbox.py
+++ sandbox.py
@@ -1,68 +1,56 @@
-# This will be the sandbox in which the first performance data will be collected.
-import os
+# Copyright (c) 2020 Angelina Horn
 import logging
-import yaml
-from kubernetes import client, config
-import docker
+import os
 from pathlib import Path
+
+import docker
+from kubernetes_py import K8sConfig, K8sContainer, K8sDeployment, K8sService
 
 # deployment path
 deployment_path = os.path.join(os.getcwd(), "deployment.yaml")
 
 
-def deploy():
+def deploy_to_cluster(name: str, port: int, image: str) -> str:
     # load config
-    config.load_kube_config()
-    # load deployment.yaml
+    # cfg_default = K8sConfig()
+    cfg_cert = K8sConfig(
+        kubeconfig=None,
+        api_host="127.0.0.1:49153",
+        cert=('C:/Users/Angi/.minikube/profiles/minikube/client.crt',
+              'C:/Users/Angi/.minikube/profiles/minikube/client.key')
+    )
+    print(cfg_cert.api_host)
     try:
-        with open(deployment_path) as f:
-            dep = yaml.safe_load(f)
-    except FileNotFoundError:
-        logging.error("Could not find config.")
-    k8s_apps_v1 = client.AppsV1Api()
-    # create namespaced deployment
-    try:
-        resp = k8s_apps_v1.create_namespaced_deployment(
-            body=dep, namespace="default")
+        # create container
+        container = K8sContainer(name=name, image=image)
+        container.add_port(
+            container_port=port,
+            host_port=port,
+            name=name
+        )
+        # create deployment
+        # deployment = K8sDeployment(
+        #     config=cfg_cert,
+        #     name=name,
+        #     replicas=1
+        # )
+        # deployment.add_container(container)
+        # deployment.create()
+        # create service
+        svc = K8sService(config=cfg_cert, name=name)
+        svc.add_port(name=f"{name}-port", port=port, target_port=port, protocol="NodePort")
+        svc.add_selector(selector=dict(name=name))
+        svc.set_cluster_ip('192.168.1.100')
+        svc.create()
+        return "True"
     except Exception as e:
         logging.error(f"Error while deploying: {e}")
-    print("Deployment created. status='%s'" % resp.metadata.name)
-
-
-def write_config(**kwargs):
-    template = """
-    apiVersion: v1
-    kind: pod
-    metadata:
-      name: {name}
-    spec:
-      replicas: {replicas}
-      template:
-        metadata:
-          labels:
-            run: {name}
-        spec:
-          containers:
-          - name: {name}
-            image: {image}
-            ports:
-            - containerPort: {port}"""
-    try:
-        with open(deployment_path, 'w') as file:
-            file.write(template.format(**kwargs))
-    except IOError:
-        logging.error("IOError while writing config.")
-
-
-def execute(name: str, port: int, docker_path: str):
-    image = build_image(name, docker_path)
-    # write_config(name=name, image=image, replicas=1, port=str(port))
-    # deploy()
 
 
 def build_image(name: str, docker_path: str) -> str:
     # init
     try:
+        image = f"{name}:latest"
         directory_path = Path(docker_path).parent
         if Path(docker_path).exists() and directory_path.exists():
             pass
@@ -71,15 +59,34 @@ def build_image(name: str, docker_path: str) -> str:
     except FileNotFoundError:
         logging.error("Could not find Dockerfile.")
     try:
-        docker_client = docker.APIClient(base_url='unix://var/run/docker.sock')
+        # docker_client = docker.APIClient(base_url='unix://var/run/docker.sock')
+        docker_client = docker.APIClient(base_url='tcp://localhost:2375')
         # build image
-        build = docker_client.build(path=str(directory_path), tag="sandbox", dockerfile=docker_path)
+        build = docker_client.build(path=str(directory_path), tag=name, dockerfile=docker_path)
         print(list(build))
-        return "Yes"
-    except Exception as e:
+        return image
+    except docker.errors.BuildError as e:
         logging.error(f"Could not build image: {e}")
+    except docker.errors.APIError as e:
+        logging.error(f"Error while using Docker API: {e}")
 
 
-# test
+def execute(name: str, port: int, docker_path: str, route: str, input_type: str, testfile_path: str) -> bool:
+    try:
+        image = build_image(name, docker_path)
+        print(f"Image name: {image}")
+        host = deploy_to_cluster(name=name, port=port, image=image)
+        print(host)
+        # benchmark.configBenchmark(host=host, route=route, input_type=input_type, testfile_path=testfile_path)
+        # benchmark.startBenchmark()
+        return True
+    except Exception as e:
+        logging.error(f"Error while executing: {e}")
+        return False
+
+
 if __name__ == '__main__':
-    write_config(name="someName", image="myImg", replicas="many", port="1111")
+    docker_path = os.path.join(os.getcwd(), "webservice", "Dockerfile")
+    testfile_path = os.path.join(os.getcwd(), "data", "test.txt")
+    execute(name="test", port=80, docker_path=docker_path, route="/healthcheck", input_type="",
+            testfile_path=testfile_path)

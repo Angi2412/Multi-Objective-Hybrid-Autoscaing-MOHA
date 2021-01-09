6de7b449e319d7cc47f101a37d936ba4b37c9011
Angelina Horn <horn.angelina96@gmail.com>
2021-01-09 14:04:56 +0100

    automated sandbox and benchmark

---
+++ benchmark.py
@@ -0,0 +1,74 @@
+from gevent import monkey
+
+monkey.patch_all()
+import os
+
+import gevent
+from dotenv import load_dotenv
+from locust import HttpUser, task
+from locust.env import Environment
+from locust.log import setup_logging
+from locust.stats import stats_history, StatsCSVFileWriter
+
+setup_logging("INFO", None)
+
+
+class User(HttpUser):
+    # init
+    load_dotenv()
+    host = f"http://{os.getenv('HOST')}:{os.getenv('PORT')}/"
+    route = os.getenv("ROUTE")
+    testfile_path = os.path.join(os.getcwd(), "data", "loadtest", f"{os.getenv('TESTFILE')}.txt")
+
+    @task
+    def my_task(self):
+        # read testfile as list
+        testfile = open(self.testfile_path, "r")
+        test_input = testfile.readlines()
+        # call route for every test point
+        for i in test_input:
+            self.client.get(f"{self.route}/{int(i)}")
+
+    @task
+    def healthcheck(self):
+        self.client.get("healthcheck")
+
+
+def start_locust():
+    gevent.monkey.patch_all()
+    # setup Environment and Runner
+    env = Environment(user_classes=[User])
+    env.create_local_runner()
+
+    # CSV writer
+    load_dotenv()
+    date = str(os.getenv('DATE'))
+    stats_path = os.path.join(os.getcwd(), "data", "raw", f"{date}_locust")
+    csv_writer = StatsCSVFileWriter(
+        environment=env,
+        base_filepath=stats_path,
+        full_history=True,
+        percentiles_to_report=[90.0, 95.0]
+    )
+
+    # start a WebUI instance
+    env.create_web_ui(host="127.0.0.1", port=8089, stats_csv_writer=csv_writer)
+
+    # start a greenlet that save current stats to history
+    gevent.spawn(stats_history, env.runner)
+
+    # spawn csv writer
+    gevent.spawn(csv_writer)
+
+    # start the test
+    env.runner.start(user_count=int(os.getenv("USERS")), spawn_rate=int(os.getenv("SPAWN_RATE")))
+
+    # stop the runner in a given time
+    time_in_seconds = (int(os.getenv("HH")) * 60 * 60) + (int(os.getenv("MM")) * 60)
+    gevent.spawn_later(time_in_seconds, lambda: env.runner.quit())
+
+    # wait for the greenlets
+    env.runner.greenlet.join()
+
+    # stop the web server for good measures
+    env.web_ui.stop()
--- sandbox.py
+++ sandbox.py
@@ -1,56 +1,36 @@
 # Copyright (c) 2020 Angelina Horn
-import datetime
+from gevent import monkey
+
+monkey.patch_all()
 import datetime as dt
 import logging
 import os
 import platform
-import subprocess
-import time
 from pathlib import Path
 
 import docker
 from dotenv import load_dotenv, set_key
 from kubernetes import client, config
-from prometheus_api_client import PrometheusConnect, MetricRangeDataFrame, Metric
+from prometheus_api_client import PrometheusConnect, MetricRangeDataFrame
+
+from benchmark import start_locust
+import time
 
 # deployment
 load_dotenv()
 NAMESPACE = os.getenv("NAMESPACE")
 DEPLOYMENT_NAME = os.getenv("DEPLOYMENT")
-DESIRED_PORT = int(os.getenv("PORT"))
+TARGET_PORT = os.getenv("PORT")
 ROUTE = os.getenv("ROUTE")
 HOST = os.getenv("HOST")
+HH = int(os.getenv("HH"))
+MM = int(os.getenv("MM"))
 
 # init logger
 logging.getLogger().setLevel(logging.INFO)
 
 
-def forward_port(port: int) -> None:
-    """
-    Forwards port of given pod to a given port.
-    :param port: port of pod
-    :return: None
-    """
-    # init
-    config.load_kube_config()
-    v1 = client.CoreV1Api()
-    ret = v1.list_namespaced_pod(watch=False, namespace=NAMESPACE)
-    pod_name = DEPLOYMENT_NAME
-    # search for pod name
-    for i in ret.items:
-        if DEPLOYMENT_NAME in i.metadata.name:
-            pod_name = i.metadata.name
-            break
-    # forward port
-    if pod_name is not DEPLOYMENT_NAME:
-        os.system(f"kubectl port-forward -n {NAMESPACE} {pod_name} {port}:{DESIRED_PORT} &")
-        # list_files = subprocess.run(["kubectl", "port-forward","-n", NAMESPACE, pod_name, f"{port}:{DESIRED_PORT}"])
-        logging.info("Port was forwarded.")
-    else:
-        logging.error("Could not find pod name.")
-
-
-def deploy_to_cluster(name: str, port: int, image: str) -> None:
+def k8s_deployment(name: str, port: int, image: str) -> None:
     """
     Creates a kubernetes deployment and pushes it into the cluster.
     :param name: name of the deployment
@@ -62,6 +42,8 @@ def deploy_to_cluster(name: str, port: int, image: str) -> None:
     config.load_kube_config()
     apps_v1 = client.AppsV1Api()
 
+    # create namespace
+    os.system("kubectl create namespace sandbox")
     # Configure Pod template container
     container = client.V1Container(
         name=name,
@@ -75,7 +57,7 @@ def deploy_to_cluster(name: str, port: int, image: str) -> None:
     )
     # Create and configure a spec section
     template = client.V1PodTemplateSpec(
-        metadata=client.V1ObjectMeta(labels={"app": name}),
+        metadata=client.V1ObjectMeta(labels={"app": name}, annotations={"linkerd.io/inject": "enabled"}),
         spec=client.V1PodSpec(containers=[container]))
     # Create the specification of deployment
     spec = client.V1DeploymentSpec(
@@ -93,28 +75,74 @@ def deploy_to_cluster(name: str, port: int, image: str) -> None:
         api_response = apps_v1.create_namespaced_deployment(
             body=deployment,
             namespace=NAMESPACE)
-        logging.info("Deployment created. status='%s'" % str(api_response.status))
+        logging.info(f"Successfully deployed deployment: {api_response.status}.")
     except Exception as e:
         logging.info(f"Error while deployment: {e}")
 
 
-def delete_deployment(api_instance: client.AppsV1Api) -> None:
+def k8s_service(app_port: int, app_name: str) -> None:
     """
-    Deletes a given deployment.
-    :param api_instance: kubernetes api instance
+    Creates and deploys a service to kubernetes.
+    :param app_port: port of the app
+    :param app_name: name of the app
     :return: None
     """
-    # Delete deployment
-    api_response = api_instance.delete_namespaced_deployment(
-        name=DEPLOYMENT_NAME,
-        namespace=NAMESPACE,
-        body=client.V1DeleteOptions(
-            propagation_policy='Foreground',
-            grace_period_seconds=5))
-    logging.info("Deployment deleted. status='%s'" % str(api_response.status))
+    # Create Service
+    config.load_kube_config()
+    api_instance = client.CoreV1Api()
+    service = client.V1Service()  # V1Service
+
+    # Creating Meta Data
+    metadata = client.V1ObjectMeta()
+    metadata.name = "sandbox-service"
+
+    service.metadata = metadata
+
+    # Creating spec
+    spec = client.V1ServiceSpec(
+        type="NodePort",
+        ports=int(TARGET_PORT)
+    )
+
+    # Creating Port object
+    port = client.V1ServicePort(
+        protocol='TCP',
+        target_port=int(TARGET_PORT),
+        port=app_port
+    )
+
+    spec.ports = [port]
+    spec.selector = {"app": app_name}
+
+    service.spec = spec
+    try:
+        api_response = api_instance.create_namespaced_service(
+            body=service,
+            namespace=NAMESPACE
+        )
+        logging.info(f"Successfully deployed service: {api_response.status}")
+    except Exception as e:
+        logging.error("Error while deploying service: %s\n" % e)
 
 
-def build_image(name: str, docker_path: str) -> str:
+def get_k8s_app_port() -> int:
+    """
+    Returns the node port of the created service.
+    :return: node port
+    """
+    # init
+    config.load_kube_config()
+    v1 = client.CoreV1Api()
+
+    # iterate through namespaces services
+    ret = v1.list_namespaced_service(namespace=NAMESPACE)
+    for i in ret.items:
+        logging.info(i.metadata.name)
+        if os.getenv("SERVICE") in i.metadata.name:
+            return int(i.spec.ports[0].node_port)
+
+
+def build_docker_image(name: str, docker_path: str) -> str:
     """
     Builds a docker image from a given path with a given name.
     :param name: desired name of docker image
@@ -140,16 +168,16 @@ def build_image(name: str, docker_path: str) -> str:
             docker_client = docker.APIClient(base_url='unix://var/run/docker.sock')
         # build image
         build = docker_client.build(path=str(directory_path), tag=name, dockerfile=docker_path, rm=True)
-        logging.info(list(build))
         logging.info(f"Build image with tag: {image_name}")
         return image_name
     except Exception as e:
         logging.error(f"Could not build image: {e}")
 
 
-def config_locust(host: str, route: str, port: int, testfile: str) -> None:
+def config_locust(host: str, route: str, port: int, testfile: str, date: str) -> None:
     """
     Configures locust by setting environment variables.
+    :param date: timestamp
     :param host: ms host
     :param route: ms route
     :param port: ms port
@@ -164,84 +192,67 @@ def config_locust(host: str, route: str, port: int, testfile: str) -> None:
         set_key(dotenv_path=env_file, key_to_set=key, value_to_set=value)
 
 
-def start_locust(users: int, hatch: int, time_hh: int, time_mm: int) -> None:
+def get_prometheus_data(mode: str) -> None:
     """
-    Starts the locust load testing.
-    :param users: number of simulated users
-    :param hatch: number of simulated hatches
-    :param time_hh: runtime hours
-    :param time_mm: runtime minutes
-    :return: None
+    Exports metric data from prometheus to a csv file.
+    :param mode: where to save
+    :return:
     """
-    response = subprocess.call(["locust", "--csv=locust", "--headless", "-u", users, "-r", hatch, "--run-time", f"{time_hh}h{time_mm}m"])
-    logging.info(response)
-
-
-def get_prometheus_data(time_hh: int, time_mm: int) -> None:
-    """
-    Exports data from Prometheus.
-    :return: None
-    """
+    # metrics to export
+    metrics = [
+        "container_memory_usage_bytes",
+        "kube_pod_container_resource_limits_memory_bytes",
+        "container_cpu_usage_seconds_total",
+        "kube_pod_container_resource_limits_cpu_cores",
+        "request_total",
+        "response_latency_ms_bucket"
+    ]
     # get metric data
-    metrics_memory_data = get_prometheus_metric(metric_name="container_memory_usage_bytes", time_hh=time_hh,
-                                                time_mm=time_mm) + get_prometheus_metric(
-        metric_name="kube_pod_container_resource_limits_memory_bytes", time_hh=time_hh, time_mm=time_mm)
-    metrics_cpu_data = get_prometheus_metric(metric_name="container_cpu_usage_seconds_total", time_hh=time_hh,
-                                             time_mm=time_mm) + get_prometheus_metric(
-        metric_name="kube_pod_container_resource_limits_cpu_cores", time_hh=time_hh, time_mm=time_mm)
+    metrics_data = get_prometheus_metric(metric_name=metrics[0])
+    for x in range(1, len(metrics)):
+        metrics_data = metrics_data + get_prometheus_metric(metric_name=metrics[x])
+
     # convert to dataframe
-    metric_memory_df = MetricRangeDataFrame(metrics_memory_data)
-    metric_cpu_df = MetricRangeDataFrame(metrics_cpu_data)
-    # init timestamp
-    x = datetime.datetime.now()
-    x = x.strftime("%Y%m%d-%H%M%S")
+    metric_df = MetricRangeDataFrame(metrics_data)
+
+    if mode != "raw":
+        # init timestamp
+        date = dt.datetime.now()
+        date = date.strftime("%Y%m%d-%H%M%S")
+    else:
+        load_dotenv()
+        date = str(os.getenv("DATE"))
     # write to csv file
-    metric_memory_df.to_csv(rf"data\{x}_memory.csv")
-    metric_cpu_df.to_csv(rf"data\{x}_csv.csv")
+    metric_df.to_csv(rf"data\{mode}\{date}_metrics.csv")
 
 
-def get_prometheus_metric(metric_name: str, time_hh: int, time_mm: int) -> list:
+def get_prometheus_metric(metric_name: str) -> list:
     """
     Gets a given metric from prometheus in a given timeframe.
     :param metric_name: name of the metric
-    :param time_hh: hours
-    :param time_mm: minutes
     :return: metric
     """
     prom = PrometheusConnect(url=os.getenv("PROMETHEUS_HOST"), disable_ssl=True)
     metric_data = prom.get_metric_range_data(
         metric_name=metric_name,
-        start_time=(dt.datetime.now() - dt.timedelta(hours=time_hh, minutes=time_mm)),
+        start_time=(dt.datetime.now() - dt.timedelta(hours=HH, minutes=MM)),
         end_time=dt.datetime.now(),
     )
     return metric_data
 
 
 def deployment(name: str, port: int, docker_path: str):
-    image = build_image(name=name, docker_path=docker_path)
-    deploy_to_cluster(name=name, port=port, image=image)
-    time.sleep(60)
-    forward_port(port=port)
+    image = build_docker_image(name=name, docker_path=docker_path)
+    k8s_deployment(name=name, port=port, image=image)
+    k8s_service(app_port=port, app_name=name)
 
 
 def benchmark():
-    # init time
-    hh = int(os.getenv("HH"))
-    mm = int(os.getenv("MM"))
-    sleep_time = (hh * 60 * 60) + (mm * 60)
+    # init date
+    date = dt.datetime.now()
+    date = date.strftime("%Y%m%d-%H%M%S")
     # benchmark
-    #config_locust(host=HOST, route=ROUTE, port=DESIRED_PORT, testfile="test")
-    #start_locust(users=10, hatch=1, time_hh=hh, time_mm=mm)
-    #time.sleep(sleep_time)
-    get_prometheus_data(time_hh=hh, time_mm=mm)
-
-
-if __name__ == '__main__':
-    """
-    Main method for test purposes.
-    """
-    docker_path = os.path.join(os.getcwd(), "webservice", "Dockerfile")
-    testfile_path = os.path.join(os.getcwd(), "data", "test.txt")
-    #deployment(name="testmonday", port=5000, docker_path=docker_path)
-    #forward_port(port=5000)
-    benchmark()
+    config_locust(host=HOST, route=ROUTE, port=get_k8s_app_port(), testfile="test", date=date)
+    time.sleep(5)
+    start_locust()
+    get_prometheus_data(mode="raw")
